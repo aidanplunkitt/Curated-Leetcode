bbc65be61b137a1c2fe43f3ad2f9c316fd8cc3d2
Aidan Plunkitt <aidan.plunkitt@gmail.com>
2020-12-18 18:09:18 -0500

    House Robber O(1) solution

--- DynamicProgramming/house_robber.py
+++ DynamicProgramming/house_robber.py
@@ -1,11 +1,9 @@
 # https://leetcode.com/problems/house-robber/
-# O(n) time, O(n) space
+# O(n) time, O(1) space
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        max_val = [0, 0, 0] + [0] * len(nums)
-        i = 3
-        while i < len(max_val):
-            max_val[i] = nums[i-3] + max(max_val[i-2], max_val[i-3])
-            i += 1
-        return max(max_val)
+        highest = neighbor = 0
+        for val in nums:
+            neighbor, highest = highest, max(neighbor + val, highest)
+        return highest
